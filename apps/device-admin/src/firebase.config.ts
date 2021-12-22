5893de91f50e6b9d45c0339384943ca0ba69dfce
Ben Martin <the.one.martin@gmail.com>
2021-12-22 15:13:14 -0500

    Add Firebase

---
+++ apps/device-admin/src/firebase.config.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyD_W3bhfdhBAjb5pEnBCb3wU_ImYBf3TQM',
+  authDomain: 'hydro-garden-68dc5.firebaseapp.com',
+  projectId: 'hydro-garden-68dc5',
+  storageBucket: 'hydro-garden-68dc5.appspot.com',
+  messagingSenderId: '273085974014',
+  appId: '1:273085974014:web:1f2b96d63f8f716ec66caf',
+  measurementId: 'G-HF0S9X1YHT',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
