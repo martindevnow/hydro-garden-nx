120ea4f77505755d8cd3fa46e272fb3117bf38ac
Ben Martin <the.one.martin@gmail.com>
2022-05-05 13:33:15 -0400

    Update dependencies and allow saving of a room

--- apps/device-admin/src/views/room-create/room-create.tsx
+++ apps/device-admin/src/views/room-create/room-create.tsx
@@ -1,28 +1,32 @@
 import { Button } from '@hydro-garden-monorepo/web/ui-shared';
 import { addDoc, collection, Firestore } from 'firebase/firestore/lite';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { db } from '../../firebase/firebase.config';
-import { useHistory } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom';
 
 const RoomCreate = () => {
-  const history = useHistory();
+  const navigate = useNavigate();
+
   const [name, setName] = React.useState<string>('');
-  const save = async () => {
+  const save = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
     const roomsRef = collection(db, 'rooms');
-    const res = await addDoc(roomsRef, { name });
-    return;
+    await addDoc(roomsRef, { name });
+    navigate('/');
   };
+
   return (
     <div>
       <h1>Create a new Room</h1>
-      <form>
+      <form onSubmit={(e) => save(e)}>
         <label>Name: </label>
         <input
           type="text"
           onChange={(e) => setName(e.target.value)}
           value={name}
         ></input>
-        <Button onClick={() => save()}>Save</Button>
+        <Button>Save</Button>
       </form>
     </div>
   );
